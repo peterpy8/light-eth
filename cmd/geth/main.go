5f4b37b8896cf9893c1e471d29cfb68c47530d6b
vivid <vivid@vivid-VirtualBox.com>
2018-06-26 19:26:01 -0700

    delete abi and contracts

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -18,7 +18,7 @@
 package main
 
 import (
-	"encoding/hex"
+	//"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -32,7 +32,7 @@ import (
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/console"
-	"github.com/ethereum/go-ethereum/contracts/release"
+	//"github.com/ethereum/go-ethereum/contracts/release"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/eth"
@@ -261,19 +261,19 @@ func makeFullNode(ctx *cli.Context) *node.Node {
 	utils.RegisterEthService(ctx, stack, utils.MakeDefaultExtraData(clientIdentifier))
 
 	// Add the release oracle service so it boots along with node.
-	if err := stack.Register(func(ctx *node.ServiceContext) (node.Service, error) {
-		config := release.Config{
-			Oracle: relOracle,
-			Major:  uint32(utils.VersionMajor),
-			Minor:  uint32(utils.VersionMinor),
-			Patch:  uint32(utils.VersionPatch),
-		}
-		commit, _ := hex.DecodeString(gitCommit)
-		copy(config.Commit[:], commit)
-		return release.NewReleaseService(ctx, config)
-	}); err != nil {
-		utils.Fatalf("Failed to register the Geth release oracle service: %v", err)
-	}
+	//if err := stack.Register(func(ctx *node.ServiceContext) (node.Service, error) {
+	//	config := release.Config{
+	//		Oracle: relOracle,
+	//		Major:  uint32(utils.VersionMajor),
+	//		Minor:  uint32(utils.VersionMinor),
+	//		Patch:  uint32(utils.VersionPatch),
+	//	}
+	//	commit, _ := hex.DecodeString(gitCommit)
+	//	copy(config.Commit[:], commit)
+	//	return release.NewReleaseService(ctx, config)
+	//}); err != nil {
+	//	utils.Fatalf("Failed to register the Geth release oracle service: %v", err)
+	//}
 
 	return stack
 }
